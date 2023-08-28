3b4afdd2f9710214ace787188f723569d9470827
crija <crijacarla@gmail.com>
2023-08-28 16:21:33 -0300

    Add main file

---
+++ main.py
@@ -0,0 +1,10 @@
+class Main:
+    pass
+
+from heroes.hero import Hero
+from enemies.wolf import Wolf
+from enemies.bear import Bear
+
+heroi = Hero(100, 0, 5, 50)
+wolf = Wolf(100, 80, 0, 'brabo', 'morder', 80, 'branco', 1.09, True)
+bear = Bear(20, 5, 0, 'mancinho', 'rosnar', 10, 'F', 1 )
