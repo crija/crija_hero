a65d0f29cf88c38ddd4dcd60654d83207f8d49a4
crija <crijacarla@gmail.com>
2023-08-28 16:20:48 -0300

    Add game hero

---
+++ heroes/hero.py
@@ -0,0 +1,18 @@
+class Hero:
+    def __init__(self, vida, defesa, ataque, velocidade):
+        self.vida = vida
+        self.defesa = defesa
+        self.ataque = ataque
+        self.velocidade = velocidade
+
+    def atacar(self):
+        print('Atacando com a espeda')
+
+    def correr(self):
+        print('Correndo rápido')
+
+    def equipar(self):
+        print('Equipando espada')
+
+    
+    
